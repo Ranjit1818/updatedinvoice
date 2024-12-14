89c4ce108ed214243a24d3001b8c4ae7cd462b9f
Ranjit1818 <ranjeetkulkarni18@gmail.com>
2024-12-14 18:03:13 +0530

    Update InvoiceForm.js

--- frontend/src/InvoiceForm.js
+++ frontend/src/InvoiceForm.js
@@ -47,7 +47,7 @@ const InvoiceForm = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/generate-invoice",
+        "https://updatedinvoice-backend.vercel.app/api/generate-invoice",
         formData
       );
       console.log("Invoice Generated: ", response.data);
