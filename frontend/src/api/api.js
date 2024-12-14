766cf7a56dd53242700ade8cd82b717b9ae10d4e
Ranjit1818 <ranjeetkulkarni18@gmail.com>
2024-12-14 18:02:35 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance for the backend API
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api", // Base URL for the backend
+  baseURL: "https://updatedinvoice-backend.vercel.app/api", // Base URL for the backend
   headers: {
     "Content-Type": "application/json",
   },
