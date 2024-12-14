6d278a576eff86593e018931a30c82715e706447
Ranjit1818 <ranjeetkulkarni18@gmail.com>
2024-12-14 18:04:02 +0530

    Update InvoiceForm.js

--- frontend/src/components/InvoiceForm.js
+++ frontend/src/components/InvoiceForm.js
@@ -48,7 +48,7 @@ const InvoiceForm = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/generate-invoice",
+        "https://updatedinvoice-backend.vercel.app/api/generate-invoice",
         formData,
         {
           responseType: "blob", // Important for handling binary data
